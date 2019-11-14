b01ee371f084611e9e152854e25bfa6c10102b29
Sam Falkner <sam.falkner@gmail.com>
2019-11-14 06:52:30 -0700

    add the problem in the comments

--- 2019-11-12.py
+++ 2019-11-12.py
@@ -2,6 +2,31 @@
 
 from collections import defaultdict
 
+"""
+Given a string s consisting of n lowercase letters, you have to
+delete the minimum number of characters from s so that every letter
+in s appears a unique number of times. We only care about the
+occurrences of letters that appear at least once in result.
+
+ Input: "eeeeffff"
+ Output: 1
+ Why? We can delete one occurence of e or one occurence of 'f'.
+       Then one letter will occur four times and the other three times.
+
+ Input: "aabbffddeaee"
+ Output:  6
+ Why:
+   For example, we can delete all occurences of 'e' and 'f'
+   and one occurence of 'd' to obtain the word "aabbda".
+   Note that both 'e' and 'f' will occur zero times in the new word,
+   but that's fine, since we only care about the letter
+   that appear at least once.
+
+ Input: "llll"
+ Output: 0
+ Why? There is no need to delete any character.
+"""
+
 
 def solve(occur):
     rc = 0
